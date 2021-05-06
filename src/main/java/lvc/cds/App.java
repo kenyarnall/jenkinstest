6a79fa8c4ac6d13722d8a8d32111e2d26c39e1c9
yarnall <yarnall@lvc.edu>
2021-05-06 15:58:11 -0400

    clean

--- src/main/java/lvc/cds/App.java
+++ src/main/java/lvc/cds/App.java
@@ -1,16 +1,6 @@
 package lvc.cds;
 
-/**
- * Hello world!
- */
 public final class App {
-    private App() {
-    }
-
-    /**
-     * Says hello to the world.
-     * @param args The arguments of the program.
-     */
     public static void main(String[] args) {
         System.out.println("Hello World part 2!");
     }
